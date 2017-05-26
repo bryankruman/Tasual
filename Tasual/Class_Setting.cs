db4030fae594ad95522bbd8ea90656f3a44a8a25
Bryan Kruman <bryankruman@gmail.com>
2017-05-25 23:25:30 -0400

    Start adding json support for settings

--- Tasual/Class_Setting.cs
+++ Tasual/Class_Setting.cs
@@ -1,14 +1,31 @@
-﻿namespace Tasual
+﻿using Newtonsoft.Json;
+
+namespace Tasual
 {
 	public class Setting
 	{
+		[JsonProperty("style")]
 		public Styles Style { get; set; }
+
+		[JsonProperty("protocol")]
 		public Protocols Protocol { get; set; }
+
+		[JsonProperty("textfile")]
 		public string TextFile { get; set; }
+
+		[JsonProperty("launchonstartup")]
 		public bool LaunchOnStartup { get; set; }
+
+		[JsonProperty("minimizetotray")]
 		public bool MinimizeToTray { get; set; }
+
+		[JsonProperty("confirmclear")]
 		public bool ConfirmClear { get; set; }
+
+		[JsonProperty("confirmdelete")]
 		public bool ConfirmDelete { get; set; }
+
+		[JsonProperty("alwaysontop")]
 		public bool AlwaysOnTop { get; set; }
 
 		public enum TimeFormat
