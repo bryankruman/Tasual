5fbc6ba9e6c5b0ce7786984db8210fe9a6a1336c
Bryan Kruman <bryankruman@gmail.com>
2017-10-02 20:28:23 -0400

    Split off AssemblyInfo into a separate class file

--- Tasual/Form_About.cs
+++ Tasual/Form_About.cs
@@ -6,7 +6,6 @@
 // ===========================================
 
 using System;
-using System.Reflection;
 using System.Windows.Forms;
 
 namespace Tasual
@@ -16,93 +15,13 @@ namespace Tasual
 		public Form_About()
 		{
 			InitializeComponent();
-			Text = String.Format("About {0}", AssemblyTitle);
-			labelProductName.Text = AssemblyProduct;
-			labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
-			labelCopyright.Text = AssemblyCopyright;
-			LinkLabel.Text = AssemblyCompany;
+			Text = String.Format("About {0}", AssemblyInfo.Title);
+			labelProductName.Text = AssemblyInfo.Product;
+			labelVersion.Text = String.Format("Version {0}", AssemblyInfo.Version);
+			labelCopyright.Text = AssemblyInfo.Copyright;
+			LinkLabel.Text = AssemblyInfo.Company;
 			//this.textBoxDescription.Text = AssemblyDescription;
 		}
-
-		#region Assembly Attribute Accessors
-
-		public string AssemblyTitle
-		{
-			get
-			{
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
-				if (attributes.Length > 0)
-				{
-					AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
-					if (titleAttribute.Title != "")
-					{
-						return titleAttribute.Title;
-					}
-				}
-				return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
-			}
-		}
-
-		public string AssemblyVersion
-		{
-			get
-			{
-				return Assembly.GetExecutingAssembly().GetName().Version.ToString();
-			}
-		}
-
-		public string AssemblyDescription
-		{
-			get
-			{
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
-				if (attributes.Length == 0)
-				{
-					return "";
-				}
-				return ((AssemblyDescriptionAttribute)attributes[0]).Description;
-			}
-		}
-
-		public string AssemblyProduct
-		{
-			get
-			{
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
-				if (attributes.Length == 0)
-				{
-					return "";
-				}
-				return ((AssemblyProductAttribute)attributes[0]).Product;
-			}
-		}
-
-		public string AssemblyCopyright
-		{
-			get
-			{
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
-				if (attributes.Length == 0)
-				{
-					return "";
-				}
-				return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
-			}
-		}
-
-		public string AssemblyCompany
-		{
-			get
-			{
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
-				if (attributes.Length == 0)
-				{
-					return "";
-				}
-				return ((AssemblyCompanyAttribute)attributes[0]).Company;
-			}
-		}
-		#endregion
 
 		private void Button_Close_Click(object Sender, EventArgs Args)
 		{
